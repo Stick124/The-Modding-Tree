56178703370eea121e07ded8272bca8daef128c9
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-24 20:53:05 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -62,7 +62,7 @@ addLayer("p", {
             cost: new Decimal(0),
             unlocked() {return hasMilestone('p', 0)},
             effect() {
-                return (((player.p.points-player.points)/player.points)-1)/10
+                return ((player.p.points-player.points)/(1+player.points))/10
             }
         },
         12: {
