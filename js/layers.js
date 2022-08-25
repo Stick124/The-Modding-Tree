8eafdbe9f6a6fc4de4d9bec2480e008eebcd98a0
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-25 00:58:44 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -16,8 +16,7 @@ addLayer("p", {
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         if (hasUpgrade('p', 12)) mult = mult.times(2)
-        if (mult.gte(player[this.layer].softcap)) {
-            mult = 0}
+        
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
