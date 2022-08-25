521927da72ad0ea308cf4ddeaf677f58b1f450d9
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-24 23:22:08 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -124,7 +124,6 @@ addLayer("q", {
     baseResource: "signiture", // Name of resource prestige is based on
     baseAmount() {return player.p.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    canBuyMax: true,
     exponent: 1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
