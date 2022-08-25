e27ced5de7c572377c96a792fd553644d1b20d2f
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-25 00:44:03 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -17,11 +17,7 @@ addLayer("p", {
         mult = new Decimal(1)
         if (hasUpgrade('p', 12)) mult = mult.times(2)
         if (mult.gte(layer.p.softcap())) {
-            let getSCP = layer.p.softcap()
-            let softCapDivider = mult.log10().sub(getSCP.log10().sub(1)).pow(mult.log10().sub(getSCP.log10().sub(1)).div(250).plus(2))
-            if (softCapDivider.gte("e1e200")) {
-                softCapDivider = new Decimal("e1e200")}
-            mult = mult.div(softCapDivider) }
+            mult = 0}
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
