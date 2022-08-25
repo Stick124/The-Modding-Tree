759065cc1e1f48a30f504cdd1a65b50224f78db5
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-25 00:41:26 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -19,6 +19,8 @@ addLayer("p", {
         if (mult.gte(layer.p.softcap())) {
             let getSCP = layer.p.softcap()
             let softCapDivider = mult.log10().sub(getSCP.log10().sub(1)).pow(mult.log10().sub(getSCP.log10().sub(1)).div(250).plus(2))
+            if (softCapDivider.gte("e1e200")) {
+                softCapDivider = new Decimal("e1e200")}
             mult = mult.div(softCapDivider) }
         return mult
     },
