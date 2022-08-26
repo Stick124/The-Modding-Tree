707f02ace16f1bcc080ce22630a40614a4021554
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-26 16:39:50 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,3 +1,36 @@
+addLayer("f", {
+    name: "familiarity", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0), // Starting amount
+    }},
+    color: "#4BCCFF",
+    requires: new Decimal(100
+        ), // Can be a function that takes requirement increases into account
+    resource: "fimularities", // Name of prestige currency
+    baseResource: "signatures", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 3, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "q", description: "q:", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return player[this.layer].unlocked && hasUpgrade("q", 12)},
+    
+    passiveGeneration() {
+        if (hasUpgrade('q', 12))
+         return .1}
+})
 addLayer("p", {
     name: "Trade for signature", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
@@ -183,7 +216,7 @@ addLayer("e", {
         unlocked: true,
 		points: new Decimal(0), // Starting amount
     }},
-    color: "#4BDCFF",
+    color: "#4BDCAF",
     requires: new Decimal(4000
         ), // Can be a function that takes requirement increases into account
     resource: "errors", // Name of prestige currency
