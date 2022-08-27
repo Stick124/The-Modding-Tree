f2841d4e91cc16bc6fd9c3caaeedc5b54c209a27
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-08-26 22:10:10 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -208,7 +208,7 @@ addLayer("q", {
         12: {
             title: "proper Leading",
             description: "a leader has a few new responsibilities.",
-            cost: new Decimal(1),
+            cost: new Decimal(500),
             unlocked() {return hasMilestone('q', 1)}
         }
     }
@@ -247,5 +247,117 @@ addLayer("e", {
     
     passiveGeneration() {
         if (hasUpgrade('q', 12))
+         return .1}
+})
+
+addLayer("C", {
+    name: "companion", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "C", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0), // Starting amount
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(1), // Can be a function that takes requirement increases into account
+    resource: "companions", // Name of prestige currency
+    baseResource: "fimularities", // Name of resource prestige is based on
+    baseAmount() {return player.f.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        if (hasUpgrade('p', 12)) mult = mult.times(2)
+        if (player.p.points.gte(tmp.p.effect.box)) mult =mult.times(tmp.p.effect.boxcap)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "P: Lose you pellets to grow red pikmin", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    effect() {
+        box = new Decimal(300)
+        if (hasUpgrade('q', 11)) box = box.times(2)
+        return {box,
+        boxcap: tmp.p.effect.box/player.p.points,
+        boxcap2: tmp.p.effect.box/player.q.points
+        }
+    },
+    effectDescription() { // Optional text to describe the effects
+        eff = this.effect();
+        return "You've met "+format(eff.box)
+    },
+    milestones: {
+        0: {
+            requirementDescription: "your own name",
+            effectDescription: "anything can be useful, when everything else was destroyed",
+            done() { return player.p.points.gte(1) }
+        },
+        1: {
+            requirementDescription: "100 Signatures",
+            effectDescription: "Some extra word of mouth",
+            done() { return player.p.points.gte(100) }
+        },
+        10: {
+            requirementDescription: "123 waffles",
+            effectDescription: "blah",
+            done() { return player.p.points.gte(100000) }
+        }
+    },
+    upgrades: {
+        11: {
+            title: "trade your written names for a new signature",
+            description: "unlock point gain.",
+            cost: new Decimal(0),
+            unlocked() {return hasMilestone('p', 0)},
+            effect() {
+                return ((player.p.points-player.points)/(1+player.points))+.05
+            }
+        },
+        12: {
+            title: "scribbling",
+            description: "aka faster writing. <br> doubles Signature gain",
+            cost: new Decimal(20),
+            unlocked() {return hasMilestone('p', 1)}
+        },
+        13: {
+            title: "plans to escape the wreckage.",
+            description: "unlock a new layer.",
+            cost: new Decimal(250),
+            unlocked() {return hasMilestone('p', 1)}
+        },
+        14: {
+            title: "Send your red pikmin to the fire zone.",
+            description: "This is different, but still boosts your pellet gain.",
+            cost: new Decimal(9),
+            unlocked() {return hasMilestone('p', 10)},
+            effect() {
+                return player[this.layer].points.add(4).pow(0.1)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }
+        },
+        15: {
+            title: "Send your red pikmin to the pellets actual location.",
+            description: "This will triple your pellet gain... and maybe your IQ.",
+            cost: new Decimal(19),
+            unlocked() {return hasMilestone('p', 10)}
+        },
+        16: {
+            title: "Send yourself to the fire zone.",
+            description: "This is worth trying, but it's slightly more worrisome.",
+            cost: new Decimal(200),
+            unlocked() {return hasMilestone('p', 10)},
+            effect() {
+                return player[this.layer].points.add(4).pow(0.1)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x*2" }
+        }
+    },
+    passiveGeneration() {
+        if (hasUpgrade('p', 11))
          return .1}
 })
