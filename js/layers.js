1a3484617718b1911c076f399b339ed327a9d147
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-09-07 13:29:17 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -260,8 +260,7 @@ addLayer("e", {
             canAfford() { return player[this.layer].points.gte(this.cost()) },
             buy() {
                 player[this.layer].points = player[this.layer].points.sub(this.cost()),           
-                setBuyableAmount(this.layer, this.id, 
-                getBuyableAmount(this.layer, this.id).add(1))
+                setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
             },
             effect(x) {
 
