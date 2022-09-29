c2e5e4d83704b1849ed5d98aa9ed9281c9895ec1
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-09-29 11:48:49 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -263,10 +263,6 @@ addLayer("e", {
                 player.q.points = player.q.points.sub(this.cost()),           
                 setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
             },
-            buyMax(x) {
-                player.q.points = player.q.points.sub(this.cost()),           
-                setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(patch))
-            },
             purchaseLimit() {return player.e.points-5-player.e.points/100},
             effect(x) {
 
