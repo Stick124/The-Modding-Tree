47c2cd523928bf07a1e9497b669d065b3e848412
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-11-29 15:23:22 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -267,7 +267,7 @@ addLayer("e", {
     buyables: {
         11: {
             cost(x) {return new Decimal(1).mul(Decimal.pow(1, x))},
-            display() { return "extend patch" +"<br>cost: " + format(tmp[this.layer].buyables[this.id].cost) + "<br>level: " +getBuyableAmount(this.layer, this.id) + "<br>effect:" + format(buyableEffect(this.layer, this.id)) },
+            display() { return "Extend patch" +"<br>cost: " + format(tmp[this.layer].buyables[this.id].cost) + "<br>level: " +getBuyableAmount(this.layer, this.id) + "<br>effect:" + format(buyableEffect(this.layer, this.id)) },
             canAfford() { return player.q.points.gte(this.cost()) },
             buy() {
                 player.q.points = player.q.points.sub(this.cost()),           
@@ -349,7 +349,7 @@ addLayer("c", {
     buyables: {
         11: {
             cost(x) {return new Decimal(1).mul(Decimal.pow(1, x))},
-            display() { return "scavengers" + "<br>cost: " + format(tmp[this.layer].buyables[this.id].cost) + "<br>level: " +getBuyableAmount(this.layer, this.id) + "<br>effect:" + format(buyableEffect(this.layer, this.id)) },
+            display() { return "Scavengers" + "<br>cost: " + format(tmp[this.layer].buyables[this.id].cost) + "<br>level: " +getBuyableAmount(this.layer, this.id) + "<br>effect:" + format(buyableEffect(this.layer, this.id)) },
             canAfford() { return player.c.points.gte(this.cost()) },
             buy() {           
                 setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
