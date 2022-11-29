48f9a5c4b3bb63d9f45bd133eee694fee2c2e6dc
Healshadow <92285831+Stick124@users.noreply.github.com>
2022-11-29 16:35:55 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -349,6 +349,7 @@ addLayer("c", {
     buyables: {
         11: {
             cost(x) {return new Decimal(1).mul(Decimal.pow(1, x))},
+            Title() {return "Scavengers"},
             display() { return "Scavengers" + "<br>cost: " + format(tmp[this.layer].buyables[this.id].cost) + "<br>level: " +getBuyableAmount(this.layer, this.id) + "<br>effect:" + format(buyableEffect(this.layer, this.id)) },
             canAfford() { return player.c.points.gte(this.cost()) },
             buy() {           
